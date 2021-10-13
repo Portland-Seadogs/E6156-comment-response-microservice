c84b5615b14ff4edd0b1383eaf7d4b867f909c48
Vishnu Nair <vishnu.nair@columbia.edu>
2021-10-13 16:45:40 -0400

    All endpoints now work.

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request
 from flask_cors import CORS
 from application_services.art_catalog_resource import ArtCatalogResource
+from http import HTTPStatus
 import json
 import logging
 
@@ -11,25 +12,137 @@ application = app = Flask(__name__)
 CORS(app)
 
 
+def form_response_json(status, result):
+    return json.dumps({
+        "status": status,
+        "result": result
+    }, default=str)
+
+
 @app.route("/")
 def health_check():
     return "<u>Hello World</u>"
 
-@app.route("/api/orders", methods=["GET"])
-def all_orders():
-    pass
+
+@app.route("/api/orders", methods=["GET", "POST"]) # WORKS!
+def orders():
+    if request.method == "GET":
+        res = ArtCatalogResource.retrieve_all_orders()
+        return Response(
+            form_response_json("success", res),
+            status=HTTPStatus.OK,
+            content_type="application/json"
+        )
+    elif request.method == "POST":
+        order_base_info = request.get_json()
+        res = ArtCatalogResource.add_new_order(order_base_info)
+        rsp = Response(json.dumps(res), status=200, content_type="application/json")
+        return rsp
+
 
 @app.route("/api/orders/<int:order_id>", methods=["GET", "DELETE"])
-def selected_order(order_id):
-    pass
+def selected_order(order_id): # TODO: DRY refactoring
+    res = ArtCatalogResource.retrieve_single_order(order_id=order_id)
+    if request.method == "GET":
+        if res is not None:
+            return Response(
+                form_response_json("success", res),
+                status=HTTPStatus.OK,
+                content_type="application/json"
+            )
+        else:
+            return Response(
+                form_response_json("order not found", None),
+                status=HTTPStatus.NOT_FOUND,
+                content_type="application/json"
+            )
+    elif request.method == "DELETE":
+        res = ArtCatalogResource.remove_order_by_id(order_id)
+        if res is not None:
+            return Response(
+                form_response_json("success", {"order_id": order_id}),
+                status=HTTPStatus.OK,
+                content_type="application/json"
+            )
+        else:
+            return Response(
+                form_response_json("order not found", None),
+                status=HTTPStatus.NOT_FOUND,
+                content_type="application/json"
+            )
+    else: # TODO: Needed?
+        return Response(
+            form_response_json("unsupported method", None),
+            status=HTTPStatus.METHOD_NOT_ALLOWED,
+            content_type="application/json"
+        )
+
 
 @app.route("/api/orders/<int:order_id>/orderitems", methods=["GET"])
-def all_items_for_order(order_id):
-    pass
+def all_items_for_order(order_id): # WORKS!
+    res = ArtCatalogResource.retrieve_all_items_in_given_order(order_id=order_id)
+    if res is None:
+        return Response(
+            form_response_json("order not found", None),
+            status=HTTPStatus.NOT_FOUND,
+            content_type="application/json"
+        )
+    else:
+        return Response(
+            form_response_json("success", res),
+            status=HTTPStatus.OK,
+            content_type="application/json"
+        )
 
-@app.route("/api/orders/<int:order_id>/orderitems/<int:item_id>", methods=["GET", "POST", "PUT", "DELETE"])
-def item_in_order(order_id, item_id):
-    pass
+
+@app.route("/api/orders/<int:order_id>/orderitems/<int:item_id>", methods=["GET", "POST", "DELETE"])
+def item_in_order(order_id, item_id): # TODO: DRY refactoring.
+    if request.method == "GET":
+        res = ArtCatalogResource.retrieve_single_item_in_given_order(order_id, item_id)
+        if res is None or res is False:
+            return Response(
+                form_response_json("order not found" if res is None else "item not in specified order", None),
+                status=HTTPStatus.NOT_FOUND,
+                content_type="application/json"
+            )
+        else: # if valued
+            return Response(
+                form_response_json("success", res),
+                status=HTTPStatus.OK,
+                content_type="application/json"
+            )
+    elif request.method == "POST": # both to create an entry and to update it
+        order_info = request.get_json() # item info in body
+        order_info["order_id"] = order_id
+        order_info["item_id"] = item_id
+        res = ArtCatalogResource.add_item_to_order(order_info)
+        if res is None:
+            return Response(
+                form_response_json("order not found", None),
+                status=HTTPStatus.NOT_FOUND,
+                content_type="application/json"
+            )
+        else:  # if valued
+            return Response(
+                form_response_json("success", res),
+                status=HTTPStatus.OK,
+                content_type="application/json"
+            )
+    elif request.method == "DELETE":
+        res = ArtCatalogResource.remove_item_from_order(order_id, item_id)
+        if res is None or res is False:
+            return Response(
+                form_response_json("order not found" if res is None else "item not in specified order", None),
+                status=HTTPStatus.NOT_FOUND,
+                content_type="application/json"
+            )
+        else:  # if valued
+            return Response(
+                form_response_json("success", {"order_id": order_id, "item_id": item_id}),
+                status=HTTPStatus.OK,
+                content_type="application/json"
+            )
+
 
 ## OLD ROUTES (for reference, remove later)
 
--- application_services/art_catalog_resource.py
+++ application_services/art_catalog_resource.py
@@ -4,39 +4,163 @@ import database_services.rdb_service as d_service
 
 class ArtCatalogResource(BaseApplicationResource):
     db_schema = "art_catalog"
-    table_name = "products"
+    order_record_table = "orders"
+    order_contents_table = "order_items"
 
     def __init__(self):
         super().__init__()
 
-    def get_links(self, resource_data):
-        pass
 
-    @classmethod
-    def retrieve_all_records(cls):
-        return d_service.fetch_all_records(cls.db_schema, cls.table_name)
+    ## ORDERS #
 
-    @classmethod
-    def retrieve_single_record(cls, record_id):
-        return d_service.find_by_template(
-            cls.db_schema, cls.table_name, {"item_id": record_id}
+    @classmethod # DONE, WORKS!
+    def retrieve_all_orders(cls):
+        found_orders = d_service.fetch_all_records(cls.db_schema, cls.order_record_table)
+        for order in found_orders:
+            order["links"] = cls.retrieve_all_items_in_given_order(order["order_id"], href=True)
+        return found_orders
+
+    @classmethod # DONE, WORKS!
+    def retrieve_single_order(cls, order_id):
+        found_orders = d_service.find_by_template(
+            cls.db_schema, cls.order_record_table, {"order_id": order_id}
         )
+        if len(found_orders) > 0:
+            found_order = found_orders[0]
+            found_order["links"] = cls.retrieve_all_items_in_given_order(found_order["order_id"], href=True)
+            return found_order
+        else:
+            return None
 
-    @classmethod
-    def add_new_product(cls, item_information):
+    @classmethod # DONE, WORKS!
+    def add_new_order(cls, order_information): ## TODO: Create order info beforehand.
         new_record_id = d_service.create_new_record(
-            cls.db_schema, cls.table_name, **item_information
+            cls.db_schema, cls.order_record_table, **order_information
         )
-        return {"item_id": new_record_id}
+        return {"order_id": new_record_id}
 
-    @classmethod
-    def update_item_by_id(cls, item_id, updated_information):
-        return d_service.update_record(
-            cls.db_schema, cls.table_name, "item_id", item_id, **updated_information
+    @classmethod # DONE
+    def remove_order_by_id(cls, order_id):
+        # check if order exists
+        found_orders = d_service.find_by_template(
+            cls.db_schema, cls.order_record_table, {"order_id": order_id}
         )
 
-    @classmethod
-    def delete_item_by_id(cls, record_id):
-        return d_service.delete_record_by_key(
-            cls.db_schema, cls.table_name, "item_id", record_id
+        # if order does not exist, return None
+        if len(found_orders) == 0:
+            return None
+
+        all_items_in_order = cls.retrieve_all_items_in_given_order(order_id, href=False)
+
+        # delete all items in order before deleting order itself
+        for i in all_items_in_order:
+            cls.remove_item_from_order(i["order_id"], i["item_id"])
+
+        return d_service.delete_record_by_multikey(
+            cls.db_schema, cls.order_record_table, order_id=order_id
         )
+
+    # TODO: Need to update order record itself in any way? (probably not)
+
+    ## ORDER CONTENTS ##
+
+    @classmethod # DONE
+    def add_item_to_order(cls, item_order_information):
+        # TODO: Check data validity?
+
+        removal_result = cls.remove_item_from_order(
+            item_order_information["order_id"],
+            item_order_information["item_id"]
+        )
+
+        # order does not exist
+        if removal_result is None:
+            return None
+
+        # create new order_item entry, using provided data.
+        new_record_id = d_service.create_new_record(
+            cls.db_schema, cls.order_contents_table, **item_order_information
+        )
+
+        return {
+            "order_id": item_order_information["order_id"],
+            "item_id": item_order_information["item_id"],
+            "record_id": new_record_id
+        }
+
+    @classmethod # DONE, WORKS!
+    def retrieve_all_items_in_given_order(cls, order_id, href=False):
+        if href:
+            all_items_in_order = d_service.find_by_template(
+                cls.db_schema, cls.order_contents_table, {"order_id": order_id}
+            )
+
+            retval = []
+            for i in all_items_in_order:
+                retval.append({
+                    "href": f'/orders/{order_id}/orderitems/{i["item_id"]}',
+                    "rel": "order_item"
+                })
+            return retval
+        else:
+            # check if order exists
+            found_orders = d_service.find_by_template(
+                cls.db_schema, cls.order_record_table, {"order_id": order_id}
+            )
+
+            # if order does not exist, return None
+            if len(found_orders) == 0:
+                return None
+
+            # else return the items in this order. could be an empty array if the order does not yet have any entries
+            return d_service.find_by_template(
+                cls.db_schema, cls.order_contents_table, {"order_id": order_id}
+            )
+
+
+    @classmethod  # DONE, WORKS!
+    def retrieve_single_item_in_given_order(cls, order_id, item_id):
+        # check if order exists
+        found_orders = d_service.find_by_template(
+            cls.db_schema, cls.order_record_table, {"order_id": order_id}
+        )
+
+        # if order does not exist, return None
+        if len(found_orders) == 0:
+            return None
+
+        # get item with ID in order with ID
+        item_in_order = d_service.find_by_template(
+            cls.db_schema, cls.order_contents_table, {"order_id": order_id, "item_id": item_id}
+        )
+
+        if item_in_order is None or len(item_in_order) == 0:
+            return False
+        else:
+            return item_in_order[0]
+
+
+    @classmethod # DONE
+    def remove_item_from_order(cls, order_id, item_id):
+        # check if order exists
+        found_orders = d_service.find_by_template(
+            cls.db_schema, cls.order_record_table, {"order_id": order_id}
+        )
+
+        # if order does not exist, return None
+        if len(found_orders) == 0:
+            return None
+
+        # check if item already exists in order
+        given_item = cls.retrieve_single_item_in_given_order(
+            order_id, item_id
+        )
+
+        # if an item entry exists, delete it
+        if given_item is not None and given_item is not False:
+            d_service.delete_record_by_multikey(
+                cls.db_schema, cls.order_contents_table, order_id=order_id, item_id=item_id
+            )
+            return True
+        else:
+            return False
