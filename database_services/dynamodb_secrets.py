f87c580b8ad8afbd6f813a747bf854515545de04
Vishnu Nair <vishnu.nair@columbia.edu>
2021-12-05 12:45:31 -0500

    Having secrets file use environment variables by default. Updating docker-compose.

--- database_services/dynamodb_secrets.py
+++ database_services/dynamodb_secrets.py
@@ -1,3 +1,16 @@
-AWS_ACCESS_KEY = ''
-AWS_SECRET_KEY = ''
-AWS_REGION_NAME = ''
+import os
+
+# if you would prefer to write out your keys here when developing locally,
+#   * set the following bool to be false
+#   * fill out lines 13-15 with your keys
+# REMEMBER: DO NOT COMMIT OR PUSH CHANGES TO THIS FILE!!!
+USE_ENV_VARIABLES = True
+
+if USE_ENV_VARIABLES:
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', None)
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', None)
+    AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', None)
+else:
+    AWS_ACCESS_KEY = ''
+    AWS_SECRET_KEY = ''
+    AWS_REGION_NAME = ''
